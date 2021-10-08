4967e506682bc127344b2f204b0ec62da58b83b9
Iván Francés <ivan.frances@easygoband.com>
2021-10-09 01:40:43 +0200

    mock

--- src/components/DateSelector.vue
+++ src/components/DateSelector.vue
@@ -1,6 +1,7 @@
 <template>
-  <div @click="show = true">
-    <v-menu
+  <div>
+    HOLI :D
+    <!-- <v-menu
       v-model="show"
       :close-on-content-click="false"
       transition="scale-transition"
@@ -49,7 +50,7 @@
         :first-day-of-week="moment.localeData().firstDayOfWeek()"
         :allowed-dates="allowedDates">
       </v-date-picker>
-    </v-menu>
+    </v-menu> -->
   </div>
 </template>
 
@@ -58,74 +59,74 @@ import moment from 'moment'
 
 // This component expects and emits all dates in YYYY-MM-DD format
 
-export default {
-  props: {
-    label: String,
-    icon: String,
-    initialValue: String,
-    firstAvailable: String,
-    lastAvailable: String,
-    readonly: Boolean,
-    disabled: Boolean,
-    clearable: Boolean,
-    dateOfBirth: Boolean,
-    error: Boolean,
-    errorMessage: String,
-    required: Boolean,
-    block: Boolean,
-    rules: Array,
-    allowedDates: Function,
-    loading: Boolean,
-    hint: String,
-    persistentHint: Boolean
-  },
-  data () {
-    return {
-      moment,
-      selectedDate: null,
-      show: false
-    }
-  },
-  created () {
-    moment.locale(this.locale)
-    if (this.initialValue) {
-      this.selectedDate = this.initialValue
-    }
-  },
-  computed: {
-    locale () {
-      return this.$i18n.locale
-    },
-    formattedDate: {
-      get () {
-        return this.selectedDate
-          ? moment(this.selectedDate, 'YYYY-MM-DD').format('DD/MM/YYYY')
-          : null
-      },
-      set (value) {
-        this.selectedDate = value
-      }
-    }
-  },
-  watch: {
-    selectedDate (value) {
-      if (!this.initialValue || (this.initialValue && (value !== this.initialValue))) {
-        this.$emit('change', value)
-      }
-    },
-    show (val) {
-      if (this.dateOfBirth) {
-        val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
-      }
-    },
-    initialValue: {
-      immediate: true,
-      handler: function (val) {
-        if (val) {
-          this.selectedDate = val
-        }
-      }
-    }
-  }
-}
+// export default {
+//   props: {
+//     label: String,
+//     icon: String,
+//     initialValue: String,
+//     firstAvailable: String,
+//     lastAvailable: String,
+//     readonly: Boolean,
+//     disabled: Boolean,
+//     clearable: Boolean,
+//     dateOfBirth: Boolean,
+//     error: Boolean,
+//     errorMessage: String,
+//     required: Boolean,
+//     block: Boolean,
+//     rules: Array,
+//     allowedDates: Function,
+//     loading: Boolean,
+//     hint: String,
+//     persistentHint: Boolean
+//   },
+//   data () {
+//     return {
+//       moment,
+//       selectedDate: null,
+//       show: false
+//     }
+//   },
+//   created () {
+//     moment.locale(this.locale)
+//     if (this.initialValue) {
+//       this.selectedDate = this.initialValue
+//     }
+//   },
+//   computed: {
+//     locale () {
+//       return this.$i18n.locale
+//     },
+//     formattedDate: {
+//       get () {
+//         return this.selectedDate
+//           ? moment(this.selectedDate, 'YYYY-MM-DD').format('DD/MM/YYYY')
+//           : null
+//       },
+//       set (value) {
+//         this.selectedDate = value
+//       }
+//     }
+//   },
+//   watch: {
+//     selectedDate (value) {
+//       if (!this.initialValue || (this.initialValue && (value !== this.initialValue))) {
+//         this.$emit('change', value)
+//       }
+//     },
+//     show (val) {
+//       if (this.dateOfBirth) {
+//         val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+//       }
+//     },
+//     initialValue: {
+//       immediate: true,
+//       handler: function (val) {
+//         if (val) {
+//           this.selectedDate = val
+//         }
+//       }
+//     }
+//   }
+// }
 </script>
